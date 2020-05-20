8ec281a6336ada5f0230f881b977329ab5d855fd
Geraldine claros <gclaros@unicauca.edu.co>
2020-05-19 22:10:26 -0500

    implementa matriz con offset

--- Assets/Scripts/distance.cs
+++ Assets/Scripts/distance.cs
@@ -6,55 +6,62 @@ using System.IO;
 
 public class distance : MonoBehaviour
 {
-    public GameObject firstObject;
-    public GameObject secondObject;
+    private GameObject firstObjectRouter;
+    private GameObject secondObject;
 
-    Transform transformFirstObject;
-    Transform transformSecondtObject;
+    private Transform transformFirstObjectRouter;
+    private Transform transformSecondtObject;
 
     private Vector3 positionFirstObjectRouter;
     private Vector3 positionSecondObject;
     private double distanceObjs;
 
-    public int distanceX = 5;
-    public int distanceZ = 10;
+    public int row = 200;
+    public int column = 8;
 
-    private float timeX = 0;
-    private int posZ = 0;
+    public float trainWidth = 3.4f;
+    public float trainLength = 87.2f;
 
-    public string[,] dataMatrix;
+    private string[,] dataMatrix;
 
     string path;
 
-    //Chancha hace matriz,PERO RAPIDO
+    private float cellWidth;
+    private float cellLength;
 
 
     void Start()
     {
+        dataMatrix = new string[row + 1, column + 1];
 
-        dataMatrix = new string[distanceZ + 1, distanceX + 1];
-        firstObject = GameObject.FindWithTag("Router");
+        firstObjectRouter = GameObject.FindWithTag("Router");
         secondObject = GameObject.FindWithTag("Floor");
-        transformFirstObject = firstObject.GetComponent<Transform>();
+
+        transformFirstObjectRouter = firstObjectRouter.GetComponent<Transform>();
         transformSecondtObject = secondObject.GetComponent<Transform>();
-        positionFirstObjectRouter = transformFirstObject.position;
+
+        positionFirstObjectRouter = transformFirstObjectRouter.position;
 
         path = Application.dataPath + "/Data.txt";
         File.WriteAllText(path, "Datos matrices");
         File.AppendAllText(path, "\n Posicion | Estado | Distancia\n");
 
-        for (int i = 0; i < distanceZ; i++)
-        {
-            File.AppendAllText(path,$"\n");
+        cellLength = trainLength / row;
+        cellWidth = trainWidth / column;
 
-            for (int j = 0; j < distanceX; j++)
+        Debug.Log($"{cellLength}{cellWidth}");
+
+        for (int i = 0; i < row; i++)
+        {
+            File.AppendAllText(path, $"\n");
+
+            for (int j = 0; j < column; j++)
             {
-                transformSecondtObject.position = new Vector3(j, 0.5f, -i);
+                transformSecondtObject.position = new Vector3(j*cellWidth, 0.5f, -i*cellLength);
                 positionSecondObject = transformSecondtObject.position;
                 distanceObjs = Vector3.Distance(positionFirstObjectRouter, positionSecondObject);
-                //distanceObjs = Math.Round(distanceObjs,2);
                 dataMatrix[i, j] = distanceObjs.ToString("N2");
-                Debug.Log($"{i},{j} = {dataMatrix[i, j]}");
+                //Debug.Log($"{i},{j} = {dataMatrix[i, j]}");
                 File.AppendAllText(path, $"  [ {i},{j} | {0} | {dataMatrix[i, j]}]  ");
             }
         }
@@ -63,33 +70,6 @@ public class distance : MonoBehaviour
 
     void Update()
     {
-        //     timeX += Time.deltaTime;
-        //     //timeZ += Time.deltaTime;
-        //     //transformSecondtObject.position = new Vector3(timeX, 0, -timeZ);
 
-
-        //     if (timeX < distanceX)
-        //     {
-        //         if (posZ < distanceZ)
-        //         {
-
-        //             int posX = Convert.ToInt32(timeX);
-
-        //             transformSecondtObject.position = new Vector3(posX, 0.5f, -posZ);
-        //             positionSecondObject = transformSecondtObject.position;
-        //             distanceObjs = Vector3.Distance(positionFirstObjectRouter, positionSecondObject);
-
-        //             dataMatrix[posZ, posX] = distanceObjs.ToString();
-        //             Debug.Log($"{posZ},{posX} = {dataMatrix[posZ, posX]}");
-        //             File.AppendAllText(path, $"\n  0       {posZ},{posX}       {dataMatrix[posZ, posX]}\n");
-        //         }
-
-        //     }
-        //     else
-        //     {
-        //         posZ++;
-        //         timeX = 0;
-        //     }
-        // }
     }
 }
---
+++ Assets/Scripts/distanceUpdate.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class distanceUpdate : MonoBehaviour
+{
+    public GameObject firstObject;
+    public GameObject secondObject;
+
+    Transform transformFirstObject;
+    Transform transformSecondtObject;
+
+    private Vector3 positionFirstObjectRouter;
+    private Vector3 positionSecondObject;
+    private double distanceObjs;
+
+    public int row = 200;
+    public int column = 8;
+
+    public float trainWidth = 3.4f;
+    public float trainLength = 87.2f;
+
+    private string[,] dataMatrix;
+
+    string path;
+
+    private float cellWidth;
+    private float cellLength;
+
+    private float timeX;
+    private int posZ;
+    private int posX;
+
+    public float offSetX = 0f;
+    public float offSetZ = 0f;
+
+    void Start()
+    {
+        cellLength = trainLength / row;
+        cellWidth = trainWidth / column;
+    
+
+        dataMatrix = new string[row + 1, column + 1];
+
+        firstObject = GameObject.FindWithTag("Router");
+        secondObject = GameObject.FindWithTag("Floor");
+
+        transformFirstObject = firstObject.GetComponent<Transform>();
+        transformSecondtObject = secondObject.GetComponent<Transform>();
+        positionFirstObjectRouter = transformFirstObject.position;
+
+        path = Application.dataPath + "/Data.txt";
+        File.WriteAllText(path, "Datos matrices");
+        File.AppendAllText(path, "\n Posicion | Estado | Distancia\n");
+    }
+    void Update()
+    {
+        timeX += Time.deltaTime;
+        if (timeX >= 1)
+        {
+            posX++;
+            timeX = 0;
+
+        }
+
+        if (posX < column)
+        {
+            if (posZ < row)
+            {
+                transformSecondtObject.position = new Vector3(posX * cellWidth + offSetX, 0.5f, -posZ * cellLength - offSetZ);
+                positionSecondObject = transformSecondtObject.position;
+                distanceObjs = Vector3.Distance(positionFirstObjectRouter, positionSecondObject);
+
+                dataMatrix[posZ, posX] = distanceObjs.ToString();
+                Debug.Log($"{posZ},{posX} = {dataMatrix[posZ, posX]}");
+                File.AppendAllText(path, $"\n  0       {posZ},{posX}       {dataMatrix[posZ, posX]}\n");
+
+            }
+        }
+        else
+        {
+            posZ++;
+            posX = 0;
+        }
+    }
+}
+
