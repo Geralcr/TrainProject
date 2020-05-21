d28b3c07b591ffa98dc35911fb9ba6f660218760
Geraldine claros <gclaros@unicauca.edu.co>
2020-05-21 17:49:12 -0500

    Distancia,Matrices,Archivo .txt en los 3 routers

--- Assets/Scripts/distance.cs
+++ Assets/Scripts/distance.cs
@@ -6,15 +6,19 @@ using System.IO;
 
 public class distance : MonoBehaviour
 {
-    private GameObject firstObjectRouter;
-    private GameObject secondObject;
+    public GameObject Router1;
+    public GameObject Router2;
+    public GameObject Router3;
+    public GameObject targetPoint;
 
-    private Transform transformFirstObjectRouter;
-    private Transform transformSecondtObject;
+    private Transform transformRouter1;
+    private Transform transformRouter2;
+    private Transform transformRouter3;
+    private Transform transformTragetPoint;
 
-    private Vector3 positionFirstObjectRouter;
-    private Vector3 positionSecondObject;
-    private double distanceObjs;
+    private double distanceRouter1;
+    private double distanceRouter2;
+    private double distanceRouter3;
 
     public int row = 200;
     public int column = 8;
@@ -22,63 +26,92 @@ public class distance : MonoBehaviour
     public float trainWidth = 3.4f;
     public float trainLength = 87.2f;
 
-    private string[,] dataMatrix;
+    private string[,] dataMatrixRouter1;
+    private string[,] dataMatrixRouter2;
+    private string[,] dataMatrixRouter3;
 
-    string path;
-
-    private float cellWidth;
-    private float cellLength;
+    string pathRouter1;
+    string pathRouter2;
+    string pathRouter3;
 
     private float offSetX = 0f;
     private float offSetZ = 0f;
 
     public string fileName;
 
+    private float cellWidth;
+    private float cellLength;
+
+    public float height;
 
     void Start()
     {
+        HandleInitial();
 
-        dataMatrix = new string[row + 1, column + 1];
+        cellLength = CalcularTamCelda(trainWidth, trainLength).Length;
+        cellWidth = CalcularTamCelda(trainWidth, trainLength).Width;
 
-        firstObjectRouter = GameObject.FindWithTag("Router");
-        secondObject = GameObject.FindWithTag("Floor");
-
-        transformFirstObjectRouter = firstObjectRouter.GetComponent<Transform>();
-        transformSecondtObject = secondObject.GetComponent<Transform>();
-
-        positionFirstObjectRouter = transformFirstObjectRouter.position;
-
-        path = Application.dataPath + $"/{fileName}.txt";
-        File.WriteAllText(path, "Datos matrices");
-        File.AppendAllText(path, "\n Posicion | Estado | Distancia\n");
-
-        cellLength = trainLength / row;
-        cellWidth = trainWidth / column;
-        
         offSetZ = cellLength / 2;
         offSetX = cellWidth / 2;
 
-        //Debug.Log($"{cellLength}{cellWidth}");
+        RecorrerMatriz();
+    }
 
+    Cell CalcularTamCelda(float width, float length)
+    {
+        Cell CellSize = new Cell();
+        CellSize.Width = width / column;
+        CellSize.Length = length / row;
+        return CellSize;
+    }
+
+    void RecorrerMatriz()
+    {
         for (int i = 0; i < row; i++)
         {
-            File.AppendAllText(path, $"\n");
+            File.AppendAllText(pathRouter1, $"\n");
+            File.AppendAllText(pathRouter2, $"\n");
+            File.AppendAllText(pathRouter3, $"\n");
 
             for (int j = 0; j < column; j++)
             {
-                transformSecondtObject.position = new Vector3(j * cellWidth + offSetX, 0.5f, -i * cellLength - offSetZ);
-                positionSecondObject = transformSecondtObject.position;
-                distanceObjs = Vector3.Distance(positionFirstObjectRouter, positionSecondObject);
-                dataMatrix[i, j] = distanceObjs.ToString("N2");
-                //Debug.Log($"{i},{j} = {dataMatrix[i, j]}");
-                File.AppendAllText(path, $"  [ {i},{j} | {0} | {dataMatrix[i, j]}]  ");
+                transformTragetPoint.position = new Vector3(j * cellWidth + offSetX, 0.5f + height, -i * cellLength - offSetZ);
+
+                distanceRouter1 = Vector3.Distance(transformRouter1.position, transformTragetPoint.position);
+                dataMatrixRouter1[i, j] = distanceRouter1.ToString("N2");
+                File.AppendAllText(pathRouter1, $"  [ {i},{j} | {0} | {dataMatrixRouter1[i, j]}]  ");
+
+                distanceRouter2 = Vector3.Distance(transformRouter2.position, transformTragetPoint.position);
+                dataMatrixRouter2[i, j] = distanceRouter2.ToString("N2");
+                File.AppendAllText(pathRouter2, $"  [ {i},{j} | {0} | {dataMatrixRouter2[i, j]}]  ");
+
+                distanceRouter3 = Vector3.Distance(transformRouter3.position, transformTragetPoint.position);
+                dataMatrixRouter3[i, j] = distanceRouter3.ToString("N2");
+                File.AppendAllText(pathRouter3, $"  [ {i},{j} | {0} | {dataMatrixRouter3[i, j]}]  ");
             }
         }
 
     }
 
-    void Update()
+    void HandleInitial()
     {
+        dataMatrixRouter1 = new string[row + 1, column + 1];
+        dataMatrixRouter2 = new string[row + 1, column + 1];
+        dataMatrixRouter3 = new string[row + 1, column + 1];
 
+        transformRouter1 = Router1.GetComponent<Transform>();
+        transformRouter2 = Router2.GetComponent<Transform>();
+        transformRouter3 = Router3.GetComponent<Transform>();
+
+        transformTragetPoint = targetPoint.GetComponent<Transform>();
+
+        pathRouter1 = Application.dataPath + $"/{fileName}Router1.txt";
+        File.WriteAllText(pathRouter1, "Datos matrices Router #1 ");
+
+        pathRouter2 = Application.dataPath + $"/{fileName}Router2.txt";
+        File.WriteAllText(pathRouter2, "Datos matrices Router #2 ");
+
+        pathRouter3 = Application.dataPath + $"/{fileName}Router3.txt";
+        File.WriteAllText(pathRouter3, "Datos matrices Router #3 ");
     }
 }
